66caa98f04f43f283635573a8db8f0ef42cc2b8b
joeltonluz <joelton@live.com>
2023-09-27 22:09:13 -0300

    initial commit

---
+++ AwesomeDevEvents.API/Controllers/DevEventsController.cs
@@ -0,0 +1,91 @@
+﻿using AwesomeDevEvents.API.Entities;
+using AwesomeDevEvents.API.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwesomeDevEvents.API.Controllers
+{
+    [Route("api/dev-events")]
+    [ApiController]
+    public class DevEventsController : ControllerBase
+    {
+        private readonly DevEventsDbContext _dbContext;
+
+        public DevEventsController(DevEventsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var devEvent = _dbContext.DevEvents.Where(d => !d.isDeleted).ToList();
+
+            return Ok(devEvent);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var devEvent = _dbContext.DevEvents.SingleOrDefault(d => d.Id == id);
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(devEvent);
+        }
+
+        [HttpPost]
+        public IActionResult Post(DevEvents devEvent)
+        {
+            _dbContext.DevEvents.Add(devEvent);
+
+            return CreatedAtAction(nameof(GetById), new { id = devEvent.Id }, devEvent);
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id, DevEvents input)
+        {
+            var devEvent = _dbContext.DevEvents.SingleOrDefault(d => d.Id == id);
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+
+            devEvent.Update(input.Title, input.Description, input.StartDate, input.EndDate);
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var devEvent = _dbContext.DevEvents.SingleOrDefault(d => d.Id == id);
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+
+            devEvent.Delete();
+
+            return Ok();
+        }
+
+        [HttpPost("{id}/speakers")]
+        public IActionResult PostSpeaker(Guid id, DevEventSpeaker speaker)
+        {
+            var devEvent = _dbContext.DevEvents.SingleOrDefault(d => d.Id == id);
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+
+            devEvent.Speakers.Add(speaker);
+
+            return NoContent();
+        }
+    }
+}
---
+++ AwesomeDevEvents.API/Persistence/DevEventsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AwesomeDevEvents.API.Entities;
+
+namespace AwesomeDevEvents.API.Persistence
+{
+    public class DevEventsDbContext
+    {
+        public List <DevEvents> DevEvents { get; set; }
+
+        public DevEventsDbContext()
+        {
+            DevEvents = new List<DevEvents>();
+        }
+    }
+}
